6fdf66cee60f094fc4f374cf23b90baafcd8a1cd
mariusz woda <mariusz.woda@gmail.com>
2025-04-12 02:22:08 +0200

    init commit

---
+++ src/main/java/pl/where2play/demospringboot34jdk21/DemoSpringBoot34Jdk21Application.java
@@ -0,0 +1,13 @@
+package pl.where2play.demospringboot34jdk21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot34Jdk21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBoot34Jdk21Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/where2play/demospringboot34jdk21/DemoSpringBoot34Jdk21ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.where2play.demospringboot34jdk21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBoot34Jdk21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
